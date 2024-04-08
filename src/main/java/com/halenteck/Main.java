c2f3c590ecf0bb648ca35637fca4d6e6b0958648
dilaraerbenzer <dilara.erbenzer@ug.bilkent.edu.tr>
2024-04-08 21:05:17 +0300

    Main method was created.

--- src/main/java/com/halenteck/Main.java
+++ src/main/java/com/halenteck/Main.java
@@ -1,7 +1,9 @@
 package com.halenteck;
 
+import com.halenteck.commonUI.LogInFrame;
+
 public class Main {
     public static void main(String[] args) {
-
+        LogInFrame loginFrame = new LogInFrame();
     }
 }

59a9c95e2f8b3076e86471c60058626a2ff64d52
Zeynep Uzman <idil.uzman@ug.bilkent.edu.tr>
2024-04-17 21:44:22 +0300

    Created SevenHeadedSnake.java class This class is one of the enemies of the Boss which he will face in one of the locations to get one of his items.

---
+++ src/main/java/com/halenteck/CombatGame/SevenHeadedSnake.java
@@ -0,0 +1,9 @@
+package com.halenteck.CombatGame;
+
+public class SevenHeadedSnake extends Enemy {
+
+    public SevenHeadedSnake(Player player) {
+        super(player, "Seven Headed Snake", 8, 13, 22, 3);
+
+    }
+}

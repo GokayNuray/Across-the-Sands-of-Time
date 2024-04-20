4c42050acb70df8ec5e3ada12c16b642d8269bf1
Zeynep Uzman <idil.uzman@ug.bilkent.edu.tr>
2024-04-20 16:26:39 +0300

    Created CarnivorousPlant.java class This class will create one of the enemies the fourth character, global warming character, will face.

---
+++ src/main/java/com/halenteck/CombatGame/CarnivorousPlant.java
@@ -0,0 +1,9 @@
+package com.halenteck.CombatGame;
+
+public class CarnivorousPlant extends Enemy {
+
+    public CarnivorousPlant(Player player) {
+        super(player, "Carnivorous Plant", 7, 11, 21, 3);
+
+    }
+}

5c54e5bf58a27764598b00de451609ecb7b6f356
Zeynep Uzman <idil.uzman@ug.bilkent.edu.tr>
2024-04-20 16:57:23 +0300

    Created Volcano.java class This class is added to create one of the locations the global warming character will have in one of his battles.

---
+++ src/main/java/com/halenteck/CombatGame/Volcano.java
@@ -0,0 +1,10 @@
+package com.halenteck.CombatGame;
+
+public class Volcano extends BattLocation {
+
+    public Volcano(Player player) {
+        super(player, "Volcano", new Dragon(player), "Soil");
+
+    }
+
+}

1371537643db22000509c331da0d79e5f168d181
Muhammet Furkan Demir <mfurkan5555@gmail.com>
2024-05-13 00:25:21 +0300

    Updated Bullet class. Finalised the bullet-player collision method and the update method.

--- src/main/java/com/halenteck/fpsGame/Bullet.java
+++ src/main/java/com/halenteck/fpsGame/Bullet.java
@@ -1,18 +1,13 @@
 package com.halenteck.fpsGame;
 
-import com.halenteck.render.Entity;
 import org.joml.Vector3f;
 
-import java.util.ArrayList;
-
 public class Bullet {
-    private Entity entity;
     private Vector3f position;
     private Vector3f velocity;
     private int damage;
-    private final float SPEED = 50f;
+    private static final float SPEED = 50f;
 
-    ArrayList<Bullet> bullets = new ArrayList<>();
 
     public Bullet(Vector3f startPosition, Vector3f direction, int damage) {
         this.position = new Vector3f(startPosition.x, startPosition.y, startPosition.z);
@@ -21,46 +16,34 @@ public class Bullet {
         this.damage = damage;
     }
 
-    public void setEntity(Entity entity) {
-        this.entity = entity;
-    }
-
-    public void updateBullets(float time, ArrayList<Player> targets) {
-        for (Bullet bullet : bullets) {
-            bullet.update(time);
-
-            for (Player target : targets)
-            {
-                if (isBulletHittingTarget(bullet, target))
-                {
-                    bullets.remove(bullet);
-                    target.takeDamage(bullet.getDamage());
-                    break;
-                }
-            }
+    public boolean doesBulletHitTarget(Player player) {
+        for (float i = 0; i < 100; i+= 0.1f) {
+            update(i);
+            if (isBulletHittingTarget(this, player)) return true;
         }
+        return false;
     }
 
     private boolean isBulletHittingTarget(Bullet bullet, Player target) {
-        float hitRadius = 1.0f; // Subject to change.
-        return bullet.getPosition().distance(target.getPosition()) <= hitRadius;
+        float targetX = target.getX();
+        float targetY = target.getY();
+        float targetZ = target.getZ();
+
+        float bulletX = bullet.getPosition().x;
+        float bulletY = bullet.getPosition().y;
+        float bulletZ = bullet.getPosition().z;
+
+        return (bulletX >= targetX - 0.2f && bulletX <= targetX + 0.2f) &&
+                (bulletY >= targetY && bulletY <= targetY + 1.7f ) &&
+                (bulletZ >= targetZ - 0.2f && bulletZ <= targetZ + 0.2f);
     }
 
     public void update(float time) {
         position.add(new Vector3f(velocity.mul(time)));
-        entity.move(position.x, position.y, position.z);
-    }
-
-    public ArrayList<Bullet> getBullets() {
-        return bullets;
     }
 
     public Vector3f getPosition() {
         return position;
-    }
-
-    public Vector3f getVelocity() {
-        return velocity;
     }
 
     public int getDamage() {
